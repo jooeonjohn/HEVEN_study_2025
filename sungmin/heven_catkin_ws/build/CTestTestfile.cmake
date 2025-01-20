cca3f17fcfa1807b3ea0108bfe3e3b4134d98e5c
fArlnd30 <ksm_0506@naver.com>
2025-01-19 17:49:40 -0800

    onlymy

--- sungmin/heven_catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/farlnd3000/heven_catkin_ws/src
-# Build directory: /home/farlnd3000/heven_catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("tutorial")
--- sungmin/heven_catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/farlnd3000/heven_catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/farlnd3000/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/farlnd3000/catkin_ws/devel;/opt/ros/noetic' != '' else []
